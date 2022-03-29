43c83b9f02496edca3b61d95a107328de4c48be9
reactionic127 <richard.gill127@gmail.com>
2022-03-29 16:41:29 +0800

    chore: wip-testing configuration

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from '@/pages/index';
+import Home from '@/pages/index';
 
-describe(`App`, () => {
+describe(`Home`, () => {
   it(`renders a heading`, () => {
-    render(<App />);
+    render(<Home />);
 
     const heading = screen.getByRole(`heading`, {
       name: /welcome to next\.js!/i,
--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,17 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+module.exports = {
+  // preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
   },
-  testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: ['node_modules', '.'],
+  testEnvironment: 'jsdom',
+  // moduleDirectories: ['node_modules', '.'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.svg$': 'jest-raw-loader',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  restoreMocks: true,
 };
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
